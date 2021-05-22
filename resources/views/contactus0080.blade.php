d52b33332e1cba90ef13a79ef47f8406058f2480
joshualaweigel <84610451+joshualaweigel@users.noreply.github.com>
2021-05-22 14:07:52 +0700

    Update and rename contactus0083.blade.php to contactus0080.blade.php

---
+++ resources/views/contactus0080.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact US</title>
+</head>
+<body>
+    <img src="images/img.jpeg" size="small" class="card-img-top" style="max-width: 18rem";>
+    <p>nama : Joshua Patrick Law</p>
+    <p>NBI : 1461900080</p>
+    <p>kontak(WA) : 082131415450</p>
+    <p>Email : wificeriaal@gmail.com</p>
+    <p>Instagram : Laweigel</p>
+</body>
+</html>
